1c2c6066dbedb179991616328292e3cc15a1b3fc
giovanni.buroni <giovanni.buroni@inesctec.pt>
2025-01-17 18:49:26 +0000

    update tree

---
+++ README.md
@@ -0,0 +1,41 @@
+# STACKY
+
+```
+.
+├── README.md
+├── analyze
+├── describe
+│   ├── __init__.py
+│   └── bivariate.py
+├── hypothesis
+│   ├── __init__.py
+│   ├── summary_statistics
+│   │   ├── categorical
+│   │   │   ├── more
+│   │   │   └── two
+│   │   └── continuous
+│   │       ├── more
+│   │       └── two
+│   │           ├── non-parametric
+│   │           │   └── __init__.py
+│   │           └── parametric
+│   │               ├── __init__.py
+│   │               └── ttest.py
+│   └── timeseries
+│       ├── __init__.py
+│       ├── residuals.py
+│       └── stationarity.py
+├── metrics
+│   ├── __init__.py
+│   ├── anomaly.py
+│   ├── classification.py
+│   └── regression.py
+├── poetry.lock
+├── pyproject.toml
+├── requirements.txt
+├── tests
+└── transform
+    ├── differencing.py
+    └── scale.py
+
+```
--- describe/bivariate.py
+++ describe/bivariate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -10,7 +11,7 @@ class BivariateAnalyzer:
         self.y = y
 
     def plot_kde(self) -> None:
-        sns.kdeplot(data=self.df, x=self.x, y=self.y, hue="kind", fill=True)
+        sns.kdeplot(data=self.df, x=self.x, y=self.y, fill=True)
         plt.show()
 
     def plot_pairplot(self) -> None:
@@ -26,11 +27,26 @@ class BivariateAnalyzer:
         sns.ecdfplot(data=self.df)
         plt.show()
 
+    def plot_corr(self) -> None:
+        # plot pearson, spearman and kendall correlation matrix in one figure
+        fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+        corr = pd.DataFrame(self.df.corr().iloc[0, 1].reshape(1, 1), index=[self.y], columns=[self.x])
+        sns.heatmap(corr, annot=True, ax=ax[0], cbar=False, cmap="warm")
+        ax[0].set_title("Pearson Correlation")
+        corr = pd.DataFrame(self.df.corr(method="spearman").iloc[0, 1].reshape(1, 1), index=[self.y], columns=[self.x])
+        sns.heatmap(corr, annot=True, ax=ax[1], cbar=False, cmap="warm")
+        ax[1].set_title("Spearman Correlation")
+        corr = pd.DataFrame(self.df.corr(method="kendall").iloc[0, 1].reshape(1, 1), index=[self.y], columns=[self.x])
+        sns.heatmap(corr, annot=True, ax=ax[2], cbar=False, cmap= "warm")
+        ax[2].set_title("Kendall Correlation")
+        plt.show()
+
 
 if __name__ == "__main__":
-    df = sns.load_dataset("geyser")
+    df = sns.load_dataset("geyser")[["waiting", "duration"]]
     bivariate = BivariateAnalyzer(df=df, x="waiting", y="duration")
     bivariate.plot_kde()
     bivariate.plot_pairplot()
     bivariate.plot_jointplot()
     bivariate.plot_ecdf()
+    bivariate.plot_corr()
