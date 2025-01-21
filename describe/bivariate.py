1d44145d9cca8eaf8fdd53dea526073d3a96af85
giovanni.buroni <giovanni.buroni@inesctec.pt>
2025-01-21 09:44:05 +0000

    update bivariate class

--- describe/bivariate.py
+++ describe/bivariate.py
@@ -3,14 +3,61 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
+from sklearn.feature_selection import mutual_info_regression
 
 class BivariateAnalyzer:
     " Class for analysis of two variables. "
 
-    def __init__(self, df: pd.DataFrame, x: str, y: str) -> None:
+    def __init__(self, df: pd.DataFrame, x: str, y: str, mutual_info: bool = False, mi_params: dict = None) -> None:
+        " Initialize the BivariateAnalyzer object. "
         self.df = df
         self.x = x
         self.y = y
+        self.mutual_info_enabled = mutual_info
+        self.mi_params = mi_params or {"n_neighbors":3, 'num_permutations':100, 'n_jobs':-1} 
+
+    def __repr__(self):
+        """
+        Return a string representation of the BivariateAnalyzer object.
+        """
+        return f"BivariateAnalyzer(x={self.x}, y={self.y}, mutual_info_enabled={self.mutual_info_enabled}, mi_params={self.mi_params})"
+
+    def _compute_mutual_info(self) -> tuple[float, float]:
+        """
+        Compute mutual information between two variables and a p-value.
+
+        :return: A tuple (mutual information score, p-value).
+        """
+        mi_base = self._compute_mi_score(self.df)
+        abs_mi_base = np.abs(mi_base)
+        # Permutation testing for p-value calculation
+        permuted_scores = []
+        for _ in range(self.mi_params["num_permutations"]):
+            df_permuted = self.df.copy()
+            df_permuted[self.x] = np.random.permutation(df_permuted[self.x].values)
+            mi_permuted = self._compute_mi_score(df_permuted)
+            permuted_scores.append(np.abs(mi_permuted))
+        # Calculate p-value
+        sum_  = (abs_mi_base >= np.array(permuted_scores)).sum()
+        freq_greater = sum_ / self.mi_params["num_permutations"]
+        p_value = 1 - freq_greater
+        return float(round(mi_base, 3)), float(round(p_value, 4))
+
+    def _compute_mi_score(self, df: pd.DataFrame) -> float:
+        """
+        Compute mutual information score between two variables.
+
+        :param df: The DataFrame containing the variables.
+        :return: The mutual information score.
+        """
+        return mutual_info_regression(
+            X=df[[self.x]], 
+            y=df[self.y], 
+            n_neighbors=self.mi_params["n_neighbors"], 
+            random_state=0, 
+            #n_jobs=-1#self.mi_params["n_jobs"]
+        )[0]
+
 
     def compute_association(self) -> dict:
         # Define correlation methods
@@ -25,12 +72,21 @@ class BivariateAnalyzer:
         for method, func in methods.items():
             corr, pval = func(self.df[self.x], self.df[self.y])
             self.results[method] = {
-                'correlation': float(round(corr, 3)),
+                'relation': float(round(corr, 3)),
                 'p-value': float(round(pval, 4))
             }
+
+        # Add mutual information results if enabled
+        if self.mutual_info_enabled:
+            mi_score, p_value = self._compute_mutual_info()
+            self.results['Mut Info'] = {
+                'relation': mi_score,
+                'p-value': p_value
+            }
+
         return self.results
 
-    def plot_corr_scipy(self) -> dict:
+    def plot_association(self) -> dict:
         """
         Calculate and plot the Pearson, Spearman, and Kendall correlation coefficients and their p-values.
 
@@ -54,14 +110,14 @@ class BivariateAnalyzer:
         pval_df.columns = ["critical", "method", "significant"]
 
         # Add correlation and p-value columns for plotting
-        pval_df["correlation"] = pval_df["method"].apply(lambda x: self.results[x]["correlation"])
+        pval_df["relation"] = pval_df["method"].apply(lambda x: self.results[x]["relation"])
         pval_df["p-value"] = pval_df["method"].apply(lambda x: self.results[x]["p-value"])
         # create unique coluln with method and p-value
         pval_df["method_pval"] = pval_df["method"] + " (p-value: " + pval_df["p-value"].astype(str) + ")"
 
         # plot the pval_df using a heatmap with index as critical, columns as method, and values as correlation and color as significant
         plt.figure(figsize=(10, 5))
-        sns.heatmap(pval_df.pivot(index="critical", columns="method_pval", values="correlation"),
+        sns.heatmap(pval_df.pivot(index="critical", columns="method_pval", values="relation"),
                     # green if +1, red if -1, white if 0 from negative red to positive green
                     cmap="RdYlGn",
                     center=0,  # Center the color map at zero
@@ -101,18 +157,20 @@ class BivariateAnalyzer:
 
 if __name__ == "__main__":
     # create daframe with two variables negative correlated
-    np.random.seed(0)
-    n = 1000
+    np.random.seed(42)
+    n = 10000
     waiting = np.random.poisson(5, n)
-    duration = 10 + waiting + np.random.normal(0, 1, n)
+    duration = 10 * waiting**2 + np.random.normal(0, 0.1, n)
     df = pd.DataFrame({"waiting": waiting, "duration": duration})
-    bivariate = BivariateAnalyzer(df=df, x="waiting", y="duration")
+    
+    bivariate = BivariateAnalyzer(df=df, x="waiting", y="duration", mutual_info=True)
     results = bivariate.compute_association()
     print(results)
-    # bivariate.plot_scatter()
-    # bivariate.plot_kde()
-    # bivariate.plot_pairplot()
-    # bivariate.plot_jointplot()
-    # bivariate.plot_ecdf()
-    # bivariate.plot_corr_pandas()
-    results = bivariate.plot_corr_scipy()
+    print('--'*20)
+
+    params = {'n_neighbors': 15, 'num_permutations': 100, 'num_jobs': -1}
+    bivariate = BivariateAnalyzer(df=df, x="waiting", y="duration", mutual_info=True, mi_params=params)
+    results = bivariate.compute_association()
+    print(results)
+
+    bivariate.plot_association()
