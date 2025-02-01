7951550ad18dd513e28ce96287e4c3589690bd10
giovanni.buroni <giovanni.buroni@inesctec.pt>
2025-02-01 21:18:04 +0000

    add coefs vaules

--- stacky-tall/describe/bivariate/regression.py
+++ stacky-tall/describe/bivariate/regression.py
@@ -18,10 +18,11 @@ class RegressionAnalyzer:
         """
         return f"RegressionBivariateAnalyzer(x={self.x}, y={self.y})"
 
-    def plot_regression(self, order: int = 1, robust: bool = False, ci: int = 95, lowess_regr: bool = False, lowess_smoother_res: bool = False):
+    def plot_regression(self, order: int = 1, ci: int = 95, lowess_smoother_res: bool = False):
         """
         Plot a regression plot between two variables.
         """
+        coefs = np.polyfit(self.x, self.y, order).round(3)
         # two subplots
         # regression plot
         fig, ax = plt.subplots(1, 2, figsize=(12, 6))
@@ -29,16 +30,16 @@ class RegressionAnalyzer:
         y_name = self.df.columns[1] if self.df is not None else "y"
         sns.regplot(x=self.x, y=self.y, data=self.df, 
                     order=order, 
-                    robust=robust, 
                     ci=ci, 
-                    scatter_kws={"alpha": 0.3}, 
-                    line_kws={"color": "green"}, 
-                    lowess=lowess_regr, ax=ax[0],
-                    label=f"Order: {order}")
+                    scatter_kws={"alpha": 0.1}, 
+                    line_kws={"color": "green"},
+                    ax=ax[0],
+                    label=f"coefs: {coefs[:-1][::-1]}")
         ax[0].set_xlabel(x_name)
         ax[0].set_ylabel(y_name)
-        ax[0].set_title(f"Regression plot between {x_name} and {y_name}")
+        ax[0].set_title(f"Regression plot between {x_name} and {y_name} with order: {order}")
         ax[0].grid(True)
+        ax[0].legend()
         # plot residuals
         sns.residplot(x=self.x, y=self.y, 
         data=self.df,
@@ -47,11 +48,12 @@ class RegressionAnalyzer:
             line_kws={"color": "red"}, 
             lowess=lowess_smoother_res, 
             ax=ax[1],
-            label=f"Order: {order}")
+            label=f"coefs: {coefs[:-1][::-1]}")
         ax[1].set_xlabel(x_name)
         ax[1].set_ylabel("Residuals")
-        ax[1].set_title(f"Residual plot between {x_name} and {y_name}")
+        ax[1].set_title(f"Residual plot between {x_name} and {y_name} with order: {order}")
         ax[1].grid(True)
+        ax[1].legend()
         plt.show()
 
 
@@ -63,10 +65,10 @@ if __name__=="__main__":
     # linear relationship between x and y
     y = 2*x + np.random.normal(0, 0.1, 1000)
     # cubic relationship between x and y
-    y = x**3 + np.random.normal(0, 0.1, 1000)
+    #y = x**3 + np.random.normal(0, 0.1, 1000)
     # create a dataframe xith columns names
     data = pd.DataFrame({"x": x, "y": y})
     # create a RegressionAnalyzer object
     ra = RegressionAnalyzer(x, y, data)
     # plot the regression results
-    ra.plot_regression(ci=95, lowess_regr=True, lowess_smoother_res=False)
+    ra.plot_regression(ci=95, lowess_smoother_res=False, order=3)
