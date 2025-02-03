34927fd9150cddacd87892c125478ab2a022780e
giovanni.buroni <giovanni.buroni@inesctec.pt>
2025-02-03 14:13:49 +0000

    fix MISO loader for regression

--- stacky-tall/tf/timeseries/miso_loader.py
+++ stacky-tall/tf/timeseries/miso_loader.py
@@ -3,21 +3,27 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-class MISOLoader:
-    " Class for preparing Multiple Inputs Single Output (MISO) data for training deep learning models. "
+class RegressionMISOLoader:
+    " Class for preparing Multiple Inputs Single Output (MISO) data for training deep learning regression models. "
 
-    def __init__(self, data, batch_size, window_size, target_name):
-        " Initialize the class with the data, batch size, window size, and target name. "
+    def __init__(self, data, split_size, batch_size, window_size, target_name):
+        " Initialize the class with the data, split size, batch size, window size, and target name. "
         self.data = data
         self.batch_size = batch_size
+        self.split_size = split_size
         self.window_size = window_size
         self.target_name = target_name
+
+    def _split_data(self):
+        " Split the data into training and testing. "
+        train, test = train_test_split(self.data, test_size=self.split_size)
+        return train, test
         
-    def prepare_training(self):
-        " Prepare the data for training. "
+    def _prepare_data(self, data):
+        " Prepare the data for training and validation. "
         # Create a tf.data.Dataset from the pandas dataframe
-        features = tf.data.Dataset.from_tensor_slices(self.data.drop(columns=[self.target_name]))
-        target = tf.data.Dataset.from_tensor_slices(self.data[self.target_name])
+        features = tf.data.Dataset.from_tensor_slices(data.drop(columns=[self.target_name]))
+        target = tf.data.Dataset.from_tensor_slices(data[self.target_name])
         # Window the features and target
         windowed_features = features.window(self.window_size,  drop_remainder=True)
         windowed_target = target.window(self.window_size,  drop_remainder=True)
@@ -27,10 +33,17 @@ class MISOLoader:
         # Combine the features and target
         dataset = tf.data.Dataset.zip((feat, label)) 
         return dataset.batch(self.batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+    
+    def prepare_training_validation(self):
+        " Prepare the data for training and validation. "
+        train, test = self._split_data()
+        train_data = self._prepare_data(train)
+        val_data = self._prepare_data(test)
+        return train_data, val_data
 
-    def prepare_test(self):
+    def prepare_test(self, test_data):
         " Prepare dat for testing"
-        features = tf.data.Dataset.from_tensor_slices(self.data)  # features only
+        features = tf.data.Dataset.from_tensor_slices(test_data)  # features only
         windowed_features = features.window(self.window_size,  drop_remainder=True)  # windowing features
         feat = windowed_features.flat_map(lambda window: window.batch(self.window_size)) # apply flat map
         dataset = tf.data.Dataset.zip(feat)  # zip data 
@@ -40,29 +53,29 @@ class MISOLoader:
 if __name__=="__main__":
 
     # create dataset with features and label
-    data = pd.DataFrame({'feature1': np.random.rand(100), 'feature2': np.random.rand(100), 'label': np.random.rand(100)})
-
-    # split data in traininig and testing
-    train, test = train_test_split(data, test_size=0.2)
-
-    # get x_train, y_train, x_test, y_test
-    x_train, y_train = train.drop(columns=['label']), train['label']
-    x_test, y_test = test.drop(columns=['label']), test['label']
+    train_data = pd.DataFrame({'feature1': np.random.rand(100), 'feature2': np.random.rand(100), 'label': np.random.rand(100)})
 
     # initialize instance object
-    miso = MISOLoader(data=train, batch_size=32, window_size=5, target_name='label')
+    miso = RegressionMISOLoader(data=train_data, split_size=0.2, batch_size=32, window_size=5, target_name='label')
 
-    # process data for training
-    train_data = miso.prepare_training()
+    # process data for training and validation
+    train_data, val_data = miso.prepare_training_validation()
+
+    # create test data
+    test_data = pd.DataFrame({'feature1': np.random.rand(100), 'feature2': np.random.rand(100)})
 
     # process data for testing
-    test_data = miso.prepare_test()
+    test_data = miso.prepare_test(test_data)
     
     # print shape data
     for x, y in train_data:
-        print(x.shape, y.shape)
+        print('training', x.shape, y.shape)
+        break
+
+    for x, y in val_data:
+        print('validation', x.shape, y.shape)
         break
 
     for x in test_data:
-        print(x.shape)
+        print('testing', x.shape)
         break
